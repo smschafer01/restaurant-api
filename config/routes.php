2b9572616795f33eb60bb178a08545f0b159a19d
AlekLewis1 <alekseilewis@gmail.com>
2026-05-31 10:28:49 -0400

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -21,14 +21,14 @@ return function (App $app) {
     //Route group for api/v1 pattern
     $app->group('/api/v1', function(RouteCollectorProxy $group) {
 
-        $group->group('/restaurant_chains', function(RouteCollectorProxy $group) {
-            $group->get('', 'RestaurantChains:index');
-            $group->get('/{chain_id}', 'RestaurantChains:view');
-        });
+    $group->group('/restaurant_chains', function(RouteCollectorProxy $group) {
+        $group->get('', 'RestaurantChains:index');
+        $group->get('/{chain_id}', 'RestaurantChains:view');
     });
 
-    $app->any('/{route:.*}', function (Request $request, Response $response) {
-        $response->getBody()->write("Page Not Found");
-        return $response->withStatus(404);
+    $group->group('/locations', function(RouteCollectorProxy $group) {
+        $group->get('', 'Location:index');
+        $group->get('/{location_id}', 'Location:view');
     });
-};
+
+});
